2e45657630b084ae8adba8101edc72764cd4d181
Brandon Cooke <brandoncookedev@gmail.com>
2020-02-13 20:54:02 -0500

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,10 +5,7 @@ IMAGE_NAME=resume-latex
 
 cd $BASEDIR
 docker build -t $IMAGE_NAME .
-docker run \
-	--mount type=bind,source=$(pwd)/,target=/app/current \
-	-it $IMAGE_NAME
-git add .
-git commit -m 'updated bcooke resume via CICD pipeline'
-git push origin master
+#docker run \
+#	--mount type=bind,source=$(pwd)/,target=/app/current \
+#	-it $IMAGE_NAME
 cd $CURDIR
