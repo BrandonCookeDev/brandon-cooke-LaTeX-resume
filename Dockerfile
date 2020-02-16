420469ff797eeca31d770c6c166fe150cf8223b4
Brandon Cooke <BrandonCookeDev@gmail.com>
2020-02-16 09:59:55 -0500

    update mounting in docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM bcooke91/node-imagemagik:12
 
+WORKDIR /app
 COPY . .
+RUN pwd
 RUN npm install
 
 # ENTRYPOINT npm run generate
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,5 +7,5 @@ cd $BASEDIR
 docker build -t $IMAGE_NAME .
 docker images
 docker run \
-	--mount type=bind,source=$(pwd)/,target=/ $IMAGE_NAME
+	--mount type=bind,source=$(pwd)/,target=/app $IMAGE_NAME
 cd $CURDIR
